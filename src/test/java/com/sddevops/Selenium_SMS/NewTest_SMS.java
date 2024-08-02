3cc9af4bf4dffd76ce0bafe92634c64ef9fc2dd5
Samathullah <sama94_@hotmail.com>
2024-08-02 19:51:34 +0800

    test

--- src/test/java/com/sddevops/Selenium_SMS/NewTest_SMS.java
+++ src/test/java/com/sddevops/Selenium_SMS/NewTest_SMS.java
@@ -63,7 +63,7 @@ public class NewTest_SMS {
 	@AfterTest
 	public void afterTest() {
 		 //Quit the ChromeDriver and close all associated window at the end of test
-		  webDriver.close();
+		  //webDriver.close();
 		  //test
 	}
 
