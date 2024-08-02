4c4b1f5dc3e57e0e0e4e317efd954463399fb7bc
Samathullah <sama94_@hotmail.com>
2024-08-02 15:31:36 +0800

    initial commit

---
+++ src/main/java/com/sddevops/Selenium_SMS/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.Selenium_SMS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
